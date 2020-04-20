2bfa558ee37b8e179ae049345574eca5c4dd5fef
Filipe Oliveira <filipeoliveirah@gmail.com>
2020-04-20 16:14:42 -0300

    ajust in fetch from search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,48 +1,48 @@
 <template>
-    <div id="searching">
-        <div id="exit">
-            <img src="@/assets/svg/cancel.svg" alt="Sair">
-        </div>
-        <div class="container">
-            <h2>Pesquisa no blog</h2>
-            <div class="row align-items-center">
-                <div class="col-12">
-                    <form id="search" @submit="getPosts">
-                        <p v-if="errors.length">
-                            <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
-                            <ul>
-                                <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+    <transition>
+        <div id="searching" v-if="show" class="active">
+            <div id="exit">
+                <img src="@/assets/svg/cancel.svg" alt="Sair">
+            </div>
+            <div class="container">
+                <h2>Pesquisa no blog</h2>
+                <div class="row align-items-center">
+                    <div class="col-12">
+                        <form id="search" @submit="getPosts">
+                            <p v-if="errors.length > 0">
+                                <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+                                <ul>
+                                    <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+                                </ul>
+                            </p>
+                            <input id="busca" v-model="term" type="text" name="busca" placeholder="Digite um termo de busca">
+                            <input type="submit" value="Buscar" @click.prevent="getPosts">
+                        </form>
+                    </div>
+                </div>
+
+                <div id="resultPosts">
+                    <div v-if="posts">
+                        <span> Resultados Encontrados: {{posts.length}} </span>
+                        <div class="row">
+                            <ul class="col-md-12">
+                                <li v-for="(post, index) in posts" :key="index">
+                                    <router-link :to="'/blog/' + post.slug">
+                                        {{post.title.rendered}}
+                                    </router-link>
+                                </li>
                             </ul>
-                        </p>
-
-                        <input id="busca" v-model="term" type="text" name="busca" placeholder="Digite um termo de busca">
-
-                        <input type="submit" value="Buscar" @click.prevent="getPosts">
-                    </form>
-                </div>
-            </div>
-
-            <div id="resultPosts">
-                <div v-if="posts">
-                    <span> Resultados Encontrados: {{posts.length}} </span>
-                    <div class="row">
-                        <ul class="col-md-12">
-                            <li v-for="(post, index) in posts" :key="index">
-                                <router-link :to="'/blog/' + post.slug">
-                                    {{post.title.rendered}}
-                                </router-link>
-                            </li>
-                        </ul>
+                        </div>
                     </div>
-                </div>
-                <div v-else>
-                    <div v-if="loader">
-                        <Loading/>
+                    <div v-else>
+                        <div v-if="loader">
+                            <Loading/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </transition>
 </template>
 
 <script>
@@ -59,23 +59,27 @@ export default {
             errors: [],
             term: null,
             posts: false,
-            loader: false
+            loader: false,
+            show: false,
+            totalPages: null
         }
     },
     mounted(){
-        this.exitSearch();
+        this.showSearch();
     },
     methods:{        
         getPosts(){
-            if (this.term) {
-                this.errors = [];   
+            if (this.term) { 
                 this.loader = true;
-                setTimeout(() => {
+                this.posts = false;
+                this.errors = [];
+
+                setTimeout( () => {
                     fetch(`https://www.dnadevendas.com.br/wp-json/wp/v2/posts/?search=${this.term}&per_page=10`)
                     .then(r => r.json())
                     .then(r => {
                         this.posts = r;
-                    });
+                    })
                 }, 1000);
             }         
             else{
@@ -85,24 +89,31 @@ export default {
                 }
             }                
         },
-        exitSearch(){            
+        showSearch(){
+            let searchBtn  = document.querySelector('#searchBtn');           
+            searchBtn.onclick = () =>{
+                this.show = true;
+                setTimeout( () => {
+                    this.exitSearch();
+                }, 200)
+            }
+            
+        },
+        exitSearch(){
             let searching  = document.querySelector('#searching');
             let resultPosts  = document.querySelector('#resultPosts');
             let exit  = document.querySelector('#exit'); 
-            let searchBtn  = document.querySelector('#searchBtn');
-
-            searchBtn.onclick = () =>{
-            searching.classList.add('active');
-            }
 
             resultPosts.onclick = () =>{
                 searching.classList.remove('active');
             }
 
             exit.onclick = () =>{
-                searching.classList.remove('active');
-                this.terms = null;
-                this.posts = [];
+                this.errors = [],
+                this.term = null,
+                this.posts = false,
+                this.loader = false,
+                this.show = false
             }
         }
     },
