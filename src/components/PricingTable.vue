6ec87086b1ebf9535ade1acd41ced6adb6c4000a
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-22 15:19:14 -0300

    add pricing table

--- src/components/PricingTable.vue
+++ src/components/PricingTable.vue
@@ -22,19 +22,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="row-table-item">
-                        <td class="title-row">Diagnóstico presencial</td>
-                        <td class="item-row no"></td>
-                        <td class="item-row no"></td>
-                        <td class="item-row yes"></td>
-                        <td class="item-row yes"></td>
-                    </tr>
-                    <tr class="row-table-item">
-                        <td class="title-row">Definição da metodologia de atração</td>
-                        <td class="item-row yes"></td>
-                        <td class="item-row yes"></td>
-                        <td class="item-row yes"></td>
-                        <td class="item-row yes"></td>
+                    <tr v-for="item in prices.linhas" :key="item.servico" class="row-table-item">
+                        <td class="title-row">{{ item.servico }}</td>
+                        <td v-for="planos in item.planos"
+                        :key="planos.index"
+                        :class="['item-row', planos]"></td>
                     </tr>
                 </tbody>
         </table>
@@ -45,27 +37,108 @@
 <script>
 export default {
   name: 'pricing-table',
-  mounted() {
-      tableHover();
-      function tableHover() {
-          const cel = document.querySelectorAll('td');
-          const row = document.querySelectorAll('tr');
-          for (var i = 0; i < cel.length; i++) {
-            cel[i].addEventListener('mouseover', (i) => {
-                var pos = i.target.cellIndex;
-                for (var j = 0; j < row.length; j++) {
-                    row[j].children[pos].classList.add('hover');
-                }
-            })
-            cel[i].addEventListener('mouseleave', (i) => {
-                var pos = i.target.cellIndex;
-                for (var j = 0; j < row.length; j++) {
-                    row[j].children[pos].classList.remove('hover');
-                }
-            })
-          }
-      }
+  data() {
+      return {
+        pageID: 8113,
+        prices: [],
+        check: null,
+      };
   },
+  mounted() {
+    this.getPrices();
+    this.haveInPlan();
+    // this.tableHover();
+  },
+    methods:{
+        getPrices() {
+            const ctx = this;
+            fetch(`https://www.dnadevendas.com.br/wp-json/acf/v3/pages/${ctx.pageID}/linhas`)
+            .then(r => r.json())
+            .then(r => {
+                ctx.prices = r;
+                try {
+                    ctx.prices.linhas.forEach(element => {
+                        if(element.planos.length == 3) {
+                            const list = 'none no';
+                            element.planos.splice(0, 0, list);
+                            for(var i = 0; i < element.planos.length; i++) {
+                                if(element.planos[i].indexOf('none no') < 0) {
+                                    element.planos[i] = element.planos[i] + ' yes';
+                                }
+                            }
+                        }
+                        if(element.planos.length == 2) {
+                            const list = 'none no';
+                            element.planos.splice(0, 0, list);
+                            element.planos.splice(1, 0, list);
+                            for(var j = 0; j < element.planos.length; j++) {
+                                if(element.planos[j].indexOf('none no') < 0) {
+                                    element.planos[j] = element.planos[j] + ' yes';
+                                }
+                            }
+                        }
+                        if(element.planos.length == 1) {
+                            const list = 'none no';
+                            element.planos.splice(0, 0, list);
+                            element.planos.splice(1, 0, list);
+                            element.planos.splice(2, 0, list);
+                            for(var l = 0; l < element.planos.length; l++) {
+                                if(element.planos[l].indexOf('none no') < 0) {
+                                    element.planos[l] = element.planos[l] + ' yes';
+                                }
+                            }
+                        }
+                        if(element.planos.length == 4) {
+                            for(var m = 0; m < element.planos.length; m++) {
+                                if(element.planos[m].indexOf('none no') < 0) {
+                                    element.planos[m] = element.planos[m] + ' yes';
+                                }
+                            }
+                        }
+                    });
+                } catch(e) {
+                    //do nothing
+                }
+            });
+        },
+        haveInPlan() {
+            const x = document.getElementsByClassName('item-row');
+            // x.forEach(element => {
+            //     console.log(element);
+            // });
+                console.log(x);
+
+            for (var i = 0; i < x.length; i++) {
+                // console.log(x);
+            }
+            // for (var i in td) {
+            //     console.log(document.td);
+            //     if(td.item[i].classList.contains('none')) {
+            //         td[i].classList.add('no');
+            //     } else {
+            //         td[i].classList.add('yes');
+            //     }
+            // }
+        },
+        tableHover() {
+            const cel = document.querySelectorAll('td');
+            const row = document.querySelectorAll('tr');
+            for (var i = 0; i < cel.length; i++) {
+                cel[i].addEventListener('mouseover', (i) => {
+                    var pos = i.target.cellIndex;
+                    for (var j = 0; j < row.length; j++) {
+                        row[j].children[pos].classList.add('hover');
+                    }
+                })
+                cel[i].addEventListener('mouseleave', (i) => {
+                    var pos = i.target.cellIndex;
+                    for (var j = 0; j < row.length; j++) {
+                        row[j].children[pos].classList.remove('hover');
+                    }
+                })
+            }
+        },
+    },
 };
 </script>
 
