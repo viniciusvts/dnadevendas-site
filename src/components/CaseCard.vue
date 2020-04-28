509caa867587b9d54d5808f6978611b9929afcee
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-28 15:20:18 -0300

    add new styles

--- src/components/CaseCard.vue
+++ src/components/CaseCard.vue
@@ -2,6 +2,7 @@
     <div class="card-case row">
       <div class="col-sm-12 col-md-9 img-field">
         <img class="img-notebook" src="../assets/img-notebook-alpha.png" alt="bg">
+        <!-- <div class="overlay-video"></div> -->
         <div class="iframe" v-html="obj.video"></div>
       </div>
       <div class="col-sm-12 col-md-3 content-client">
@@ -32,6 +33,15 @@ export default {
 <style lang="scss">
   @import '@/assets/scss/variables.scss';
   @import '@/assets/scss/breakpoint.scss';
+  .overlay-video {
+    position: absolute;
+    left: -40px;
+    height: 443px;
+    width: 708px;
+    top: 75px;
+    z-index: 2;
+    background-color: black;
+  }
   .content-client {
     position: relative;
     .infos {
