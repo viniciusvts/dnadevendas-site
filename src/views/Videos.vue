05fc445a0a98e4e930a3c704ab9656fa02aeb4fa
Vinicius de Santana <vinicius.vts@gmail.com>
2020-05-15 14:48:13 -0300

    set videos dinamic

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,79 +1,122 @@
 <template>
-    <section class="materials mt-5">
-        
-        <div class="container">
-            <h1>Videos</h1>
-        <p>Assista nossos especialistas apresentando dicas valiosas para melhorar a performance de seu time e aumentar sua produtividade de vendas.</p>
-        
-            <div class="row">
-                <div class="col-md-4">
-                    <router-link to="/">
-                        <div class="card">
-                            <div class="row">
-                            <div class="col-6 col-md-12 col-lg-12">
-                                <div class="left">
-                                <img src="https://www.dnadevendas.com.br/wp-content/uploads/consultoria-online-de-vendas.jpg" alt="">
-                                </div>
-                            </div>
-                            <div class="col-6 col-md-12 col-lg-12">
-                                <div class="right">
-                                <h2>Consultoria online de vendas: vantagens para pequenas e médias empresas</h2>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                    </router-link>
+  <section class="materials videos mt-5">
+    <div v-if="page" class="container">
+      <h1>{{page.title.rendered}}</h1>
+      <p>Assista nossos especialistas apresentando dicas valiosas para melhorar a performance de seu time e aumentar sua produtividade de vendas.</p>
+      
+      <ul class="menu row justify-content-center">
+          <li class="col-auto">
+              <div v-on:click="showVideos" class="tooltip active" id="todos">
+                  Todos
+              </div>                    
+          </li>
+          <li v-for="cat in getVideosCategories" :key="cat" class="col-auto">
+              <div class="tooltip" v-on:click="showVideos" :id="cleanSpaces(cat)">
+                  {{cat}}
+              </div>
+          </li>
+      </ul>
+
+      <div class="row">
+        <div v-for="(vid, index) in page.acf.videos" 
+        :key="index" 
+        :class="cleanSpaces(vid.categoria)"
+        class="col-12 col-lg-4 mb-4 videoCard active">
+          <div class="card h-100">
+            <div class="row h-100">
+              <div class="col-12">
+                <div class="left">
+                  <iframe 
+                  width="100%" 
+                  height="100%" 
+                  :src="'//www.youtube.com/embed/'+ getYtId(vid.video)"
+                  frameborder="0"
+                  allowfullscreen="">
+                  </iframe>
                 </div>
-                <div class="col-md-4">
-                    <router-link to="/">
-                        <div class="card">
-                            <div class="row">
-                            <div class="col-6 col-md-12 col-lg-12">
-                                <div class="left">
-                                <img src="https://www.dnadevendas.com.br/wp-content/uploads/consultoria-online-de-vendas.jpg" alt="">
-                                </div>
-                            </div>
-                            <div class="col-6 col-md-12 col-lg-12">
-                                <div class="right">
-                                <h2>Consultoria online de vendas: vantagens para pequenas e médias empresas</h2>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                    </router-link>
+              </div>
+              <div class="col-12 h-50">
+                <div class="right h-90">
+                  <h2>{{vid.titulo}}</h2>
                 </div>
-                <div class="col-md-4">
-                    <router-link to="/">
-                        <div class="card">
-                            <div class="row">
-                            <div class="col-6 col-md-12 col-lg-12">
-                                <div class="left">
-                                <img src="https://www.dnadevendas.com.br/wp-content/uploads/consultoria-online-de-vendas.jpg" alt="">
-                                </div>
-                            </div>
-                            <div class="col-6 col-md-12 col-lg-12">
-                                <div class="right">
-                                <h2>Consultoria online de vendas: vantagens para pequenas e médias empresas</h2>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                    </router-link>
-                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+
+    <div v-else class="container">
+      <Loading />
+    </div>
+  </section>
 </template>
 
 <script>
+import Loading from "@/components/Loading.vue";
+import Api from "@/services/ApiRest.js"; 
+
 export default {
-    name: "Videos"
+name: "Videos",
+  components:{Loading},
+  data(){
+    return {
+      pageId: 906,
+      page: null
+    }
+  },
+  created(){
+    this.getPage();
+  },
+  methods:{
+    getPage () {
+      Api.getPagesById(this.pageId)
+      .then(resp => resp.json())
+      .then(json => {
+        this.page = json;
+        this.$root.meta.title = this.page.yoast_title;
+        this.$root.meta.tags = this.page.yoast_meta;
+      });
+    },
+    showVideos (evt) {
+        let categorias = document.querySelectorAll('.tooltip');
+        let videos = document.querySelectorAll('.videoCard');
+        let item = evt.target.getAttribute('id');
+        if (videos.length == 0) return; // se não carregou ainda as imagens, faz nada
+        for(let cat of categorias){
+            if (cat.getAttribute('id') == item) cat.classList.add('active');
+            else cat.classList.remove('active');
+        }
+        for(let video of videos){
+            if(video.classList.contains(item) || item === 'todos') video.classList.add('active');
+            else video.classList.remove('active');
+        }
+    },
+    cleanSpaces(string){
+      return string.replace(/ /g,"");
+    },
+    /**https://stackoverflow.com/questions/21607808/convert-a-youtube-video-url-to-embed-code/21607897*/
+    getYtId (url) {
+      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+      const match = url.match(regExp);
+
+      return (match && match[2].length === 11)
+        ? match[2]
+        : null;
+    }
+  },
+  computed:{
+    getVideosCategories () {
+      let cats = [];
+      for (const video of this.page.acf.videos) {
+        cats.push(video.categoria);
+      }
+      return [...new Set(cats)]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-    @import '@/assets/scss/variables.scss';
-    @import '@/assets/scss/mixins.scss';
-    @import '@/assets/scss/materials.scss';
-    @import '@/assets/scss/cards.scss';
+  @import '@/assets/scss/videos.scss';
 </style>
