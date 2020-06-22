8a92c6f22cb2e9af1dcb639c42e41ab6d7c58627
Filipe Oliveira <filipeoliveirah@gmail.com>
2020-06-22 12:08:03 -0300

    add labels in contact form page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,51 +4,93 @@
     <section class="container">
       <div class="row">
         <div class="col-md-8 col-sm-12 col-tb-12">
-            <form action="">
-                <h2 class="title-message">Vamos conversar?</h2>
-                <input type="text" name="nome" id="nome" v-model="formData[0].nome" placeholder="Nome*" required>
-                <input type="text" v-model="formData[0].from" name="nome" id="" placeholder="Email*" required>
-                <select name="setor" v-model="formData[0].setor" id="setor" required>
-                    <option value="" disabled selected>Quero falar com:</option>
-                    <option value="Comercial">Comercial</option>
-                    <option value="Administrativo">Administrativo</option>
-                    <option value="Comercial 1">Comercial</option>
-                    <option value="Comercial 2">Comercial</option>
-                </select>
-                <input type="text" v-model="formData[0].tell" name="telefone" id="" placeholder="Telefone*" required>
-                <input type="text" v-model="formData[0].subject" name="assunto" id="" placeholder="Assunto*" required>
-                <textarea name="mensagem" v-model="formData[0].mensagem" id="mensagem" cols="90" rows="5" placeholder="Mensagem*" required></textarea>
+          <form action>
+            <h2 class="title-message">Vamos conversar?</h2>
+            <label for="setor">Nome*</label>
+            <input
+              type="text"
+              name="nome"
+              id="nome"
+              v-model="formData[0].nome"
+              required
+            />
+            <label for="email">E-mail*</label>
+            <input
+              type="text"
+              v-model="formData[0].from"
+              name="nome"
+              id              
+              required
+            />
+            <label for="setor">Quero falar com</label>
+            <select name="setor" v-model="formData[0].setor" id="setor" required>
+              <option value="Comercial">Comercial</option>
+              <option value="Administrativo">Administrativo</option>
+              <option value="Comercial 1">Comercial</option>
+            </select>
+            <label for="telefone">Telefone*</label>
+            <input
+              type="text"
+              v-model="formData[0].tell"
+              name="telefone"
+              id
+              required
+            />
 
-              <p>* Campos obrigatórios</p>
-                <button @click.prevent="sendMail">
-                  Enviar
-                </button>
-                <!-- <input type="submit" value="Enviar mensagem"> -->
-            </form>
+            <label for="setor">Assunto</label>
+            <input
+              type="text"
+              v-model="formData[0].subject"
+              name="assunto"
+              id
+              required
+            />
+
+            <label for="setor">Mensagem</label>
+            <textarea
+              name="mensagem"
+              v-model="formData[0].mensagem"
+              id="mensagem"
+              cols="90"
+              rows="5"
+              required
+            ></textarea>
+
+            <p>* Campos obrigatórios</p>
+            <button @click.prevent="sendMail">Enviar</button>
+            <!-- <input type="submit" value="Enviar mensagem"> -->
+          </form>
         </div>
 
         <div class="col-md-4 col-sm-12 col-tb-12">
-
           <p class="local flex-wrap" v-for="contact in contacts" :key="contact.city">
-            <img src="../assets/svg/pinmap.gold.svg" alt="" srcset="">
+            <img src="../assets/svg/pinmap.gold.svg" alt srcset />
             <b v-if="contact.city">{{contact.city}}</b>
             {{contact.end}}
-            <a v-if="contact.email" :href="'mailto:'+contact.email">{{contact.email}}</a>
+            <a
+              v-if="contact.email"
+              :href="'mailto:'+contact.email"
+            >{{contact.email}}</a>
             <a v-if="contact.telefone" :href="'tel:'+contact.telefone">{{contact.telefone}}</a>
           </p>
 
           <p class="local">
             Quer fazer parte do nosso time?
-            <a href="https://jobs.solides.com/dna" target="_blank">Trabalhe Conosco</a>
-          </p>            
+            <a
+              href="https://jobs.solides.com/dna"
+              target="_blank"
+            >Trabalhe Conosco</a>
+          </p>
         </div>
       </div>
     </section>
     <div v-html="mapIframe"></div>
     <div class="row buttons-row">
-        <button v-for="contact in contactsWithMap" 
-        :key="contact.map" 
-        @click="setMapIframe(contact.map, $event)">{{contact.city}}</button>
+      <button
+        v-for="contact in contactsWithMap"
+        :key="contact.map"
+        @click="setMapIframe(contact.map, $event)"
+      >{{contact.city}}</button>
     </div>
   </div>
 </template>
@@ -56,7 +98,7 @@
 
 <script>
 // import Api from "@/services/ApiRest.js";
-import send from '../services/Contact.js';
+import send from "../services/Contact.js";
 export default {
   name: "Contact",
   mixins: [send],
@@ -65,31 +107,36 @@ export default {
       mapIframe: null,
       contacts: [
         {
-          city: 'São Paulo',
-          end: 'Av. Maj. Sylvio de Magalhães Padilha 5200 America Business Park, Ed. Montreal 8º andar, Morumbi – São Paulo',
-          email: 'contato@dnadevendas.com.br',
-          telefone: '+55 (11) 2384-1502',
-          map: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d29244.614597326214!2d-46.703224!3d-23.619505000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4ae67492de264cb6!2sAmerica%20Business%20Park!5e0!3m2!1sen!2sus!4v1585587519596!5m2!1sen!2sus" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
+          city: "São Paulo",
+          end:
+            "Av. Maj. Sylvio de Magalhães Padilha 5200 America Business Park, Ed. Montreal 8º andar, Morumbi – São Paulo",
+          email: "contato@dnadevendas.com.br",
+          telefone: "+55 (11) 2384-1502",
+          map:
+            '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d29244.614597326214!2d-46.703224!3d-23.619505000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4ae67492de264cb6!2sAmerica%20Business%20Park!5e0!3m2!1sen!2sus!4v1585587519596!5m2!1sen!2sus" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
         },
         {
-          city: 'Belo Horizonte',
-          end: 'Rua Canopus 11, Santa Lucia, Belo Horizonte – MG',
+          city: "Belo Horizonte",
+          end: "Rua Canopus 11, Santa Lucia, Belo Horizonte – MG",
           email: null,
-          telefone: '+55 (31) 4042-0442',
-          map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.2813349957087!2d-43.94970308508518!3d-19.954667386589936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa697861f97e149%3A0xffe6369f045869b2!2sR.%20Canopus%2C%2011%20-%20Santa%20L%C3%BAcia%2C%20Belo%20Horizonte%20-%20MG%2C%2030360-112!5e0!3m2!1spt-BR!2sbr!4v1587746473646!5m2!1spt-BR!2sbr" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
+          telefone: "+55 (31) 4042-0442",
+          map:
+            '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.2813349957087!2d-43.94970308508518!3d-19.954667386589936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa697861f97e149%3A0xffe6369f045869b2!2sR.%20Canopus%2C%2011%20-%20Santa%20L%C3%BAcia%2C%20Belo%20Horizonte%20-%20MG%2C%2030360-112!5e0!3m2!1spt-BR!2sbr!4v1587746473646!5m2!1spt-BR!2sbr" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
         },
         {
-          city: 'Rio de Janeiro',
+          city: "Salvador",
+          end:
+            "R. Carlos Alberto Santos, nº7 Ed. Mais Empresarial, 5º Andar Vilas do Atlântico – Lauro de Freitas – BA",
+          email: null,
+          telefone: "+55 (71) 3289-5595",
+          map:
+            '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.582831596932!2d-38.30206118517906!3d-12.87019919092251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7163e16a0d82513%3A0x130e7ebf24ae7c63!2sMais%20Empresarial!5e0!3m2!1spt-BR!2sbr!4v1589816921620!5m2!1spt-BR!2sbr" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
+        },
+        {
+          city: "Rio de Janeiro",
           end: null,
           email: null,
-          telefone: '+55 (21) 4042-5440'
-        },
-        {
-          city: 'Salvador',
-          end: 'R. Carlos Alberto Santos, nº7 Ed. Mais Empresarial, 5º Andar Vilas do Atlântico – Lauro de Freitas – BA',
-          email: null,
-          telefone: '+55 (71) 3289-5595',
-          map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.582831596932!2d-38.30206118517906!3d-12.87019919092251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7163e16a0d82513%3A0x130e7ebf24ae7c63!2sMais%20Empresarial!5e0!3m2!1spt-BR!2sbr!4v1589816921620!5m2!1spt-BR!2sbr" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
+          telefone: "+55 (21) 4042-5440"
         }
       ],
       formData: [
@@ -99,9 +146,9 @@ export default {
           setor: null,
           tell: null,
           subject: null,
-          mensagem: null,
-        },
-      ],
+          mensagem: null
+        }
+      ]
     };
   },
   created() {
@@ -113,34 +160,36 @@ export default {
   },
   methods: {
     removeActiveButtons(first) {
-      var buttons = document.querySelectorAll('.buttons-row button');
+      var buttons = document.querySelectorAll(".buttons-row button");
       for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('active');
+        buttons[i].classList.remove("active");
       }
-      if (first && buttons.length > 0) buttons[0].classList.add('active');
+      if (first && buttons.length > 0) buttons[0].classList.add("active");
     },
     setMapIframe(iframe, event) {
-      if(typeof event == 'undefined') {
+      if (typeof event == "undefined") {
         // se não manda evento é a primeira vez
         this.removeActiveButtons(true);
       } else {
         this.removeActiveButtons(false);
-        event.target.classList.add('active')
+        event.target.classList.add("active");
       }
       this.mapIframe = iframe;
     },
-    setMeta(){
-      fetch('https://www.dnadevendas.com.br/wp-json/wp/v2/pages/?slug=contato')
-      .then(res => res.json())
-      .then(json =>{
-        this.$root.meta.title = json[0].yoast_title;
-        this.$root.meta.tags = json[0].yoast_meta;
-      })
+    setMeta() {
+      fetch("https://www.dnadevendas.com.br/wp-json/wp/v2/pages/?slug=contato")
+        .then(res => res.json())
+        .then(json => {
+          this.$root.meta.title = json[0].yoast_title;
+          this.$root.meta.tags = json[0].yoast_meta;
+        });
     }
   },
   computed: {
-    contactsWithMap: function(){ //propriedade computada
-      return this.contacts.filter(function(contact){ //filtra os contatos
+    contactsWithMap: function() {
+      //propriedade computada
+      return this.contacts.filter(function(contact) {
+        //filtra os contatos
         return contact.map; // por aqueles que possuem map
       });
     }
@@ -149,5 +198,5 @@ export default {
 </script>
 
 <style lang="scss">
-  @import '@/assets/scss/contact.scss';
+@import "@/assets/scss/contact.scss";
 </style>
