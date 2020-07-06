acf0dd61412762b273a845487cc63698883cb88c
Filipe Oliveira <filipeoliveirah@gmail.com>
2020-07-06 17:19:31 -0300

    ajust banner for small

--- src/components/BannerForSmall.vue
+++ src/components/BannerForSmall.vue
@@ -1,17 +1,24 @@
 <template>
-  <section class="container" id="details-fs">
-    <h2>Há mais de <span>14 anos</span> aumentando a
-    produtividade de vendas em mais de <span id="animation"></span></h2>
-    <img src="../assets/banner-4small.png" alt="Banner">
+  <section>
+    <div class="container-fluid" id="details-fs">
+      <h2>
+        Há mais de <span>14 anos</span> <br />
+        aumentando a <br />
+        produtividade de <br />
+        vendas em mais de <br />
+        <span id="animation"></span>
+      </h2>
+      <img src="../assets/banner-4small.png" alt="Banner" />
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'banner-4small',
+  name: "banner-4small"
 };
 </script>
 
 <style lang="scss">
-  @import '@/assets/scss/bannerForSmall.scss';
+@import "@/assets/scss/bannerForSmall.scss";
 </style>
