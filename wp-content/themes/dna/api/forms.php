e0b6302dced371fe2ded7d6d461efc921c4f00ac
Vinicius de Santana <vinicius.vts@gmail.com>
2020-08-26 15:46:53 -0300

    fix: email layout form contact page

--- wp-content/themes/dna/api/forms.php
+++ wp-content/themes/dna/api/forms.php
@@ -70,14 +70,72 @@ function dnaapi_paginaContato($req){
       'lucia@dnadevendas.com.br>'
     );
   $subject = 'DNA de Vendas - Contato';
-  $message = "Nome: ".$nome
-      ."<br>Email: ".$email
-      ."<br>setor: ".$setor
-      ."<br>nVendedores: ".$nVendedores
-      ."<br>telefone: ".$telefone
-      ."<br>assunto: ".$assunto
-      ."<br>mensagem: ".$mensagem
-      ."<br>urlOrigem: ".$urlOrigem;
+  // inicio mensagem
+  $message = '<div style="font-family: Arial, sans-serif;margin: 40px auto;width: 550px;">';
+  $message .=   '<table>';
+  $message .=     '<tbody><tr>';
+  $message .=     '<td><img src="https://www.dnadevendas.com.br/wp-content/uploads/mail_dnadevendas.png" width="100" height="45"></td>';
+  $message .=     '<td><h1 style="margin-left: 30px;font-weight: 800;margin-bottom: 0;">Solicitação de Contato</h1>';
+  $message .=     '<h3 style="font-weight: 100;margin-left: 30px;margin-top: 0;">Site DNA de Vendas</h3></td>';
+  $message .=     '</tr></tbody>';
+  $message .=   '</table>';
+  $message .=   '<table style="font-family: Arial, sans-serif;background-color: #eee;margin: 20px 40px 50px 40px;border-radius: 6px;min-width: 450px;">';
+  $message .=     '<tbody>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;width: 60px">Nome:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $nome;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;">E-mail:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $email;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;">Falar com setor:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $setor;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;">Vendedores:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $nVendedores;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;">Telefone:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $telefone;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;">Assunto:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $assunto;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;" colspan="2">';
+	$message .=         '<p style="padding-bottom: 20px;">Mensagem:</p>';
+  $message .=         '<div style="font-size: 18px;">';
+  $message .=            $mensagem;
+  $message .=          '</div>';
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '<tr>';
+  $message .=       '<td style="padding: 10px 20px;">UrlOrigem:</td>';
+  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+  $message .=         $urlOrigem;
+  $message .=       '</td>';
+  $message .=     '</tr>';
+  $message .=     '</tbody>';
+  $message .=   '</table>';
+  $message .=   '<p style="font-family: Arial, sans-serif;font-weight: 100;font-style: italic;">Enviado pelo site Consultoria DNA de Vendas.</p>';
+  $message .= '</div>';
+  // fim mensagem
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $wpmail = wp_mail( $to, $subject, $message, $headers );
   $url = $_SERVER['HTTP_ORIGIN'] . '/contato/agradecimento/';
