4561bc41de16b8f6702e6133b40d6146b6a61064
Vinicius de Santana <vinicius.vts@gmail.com>
2020-09-01 14:45:16 -0300

    fix: unify mensagens email

--- wp-content/themes/dna/api/forms.php
+++ wp-content/themes/dna/api/forms.php
@@ -6,18 +6,18 @@
  */
 function dnaapi_contatoFooter($req){
   // pega os parametros
-  $nome = $req->get_param('nome');
-  $email = $req->get_param('email');
-  $urlOrigem = $req->get_param('urlOrigem');
+  $reqData = array(
+    'urlOrigem' => $req->get_param('urlOrigem'),
+    'nome' => $req->get_param('nome'),
+    'email' => $req->get_param('email')
+  );
   //configura e envia para o rd
-  $args = configuraEnviarParaRDNewsletterFooter($nome, $email, $urlOrigem);
+  $args = configuraEnviarParaRDNewsletterFooter($reqData);
   $statusEnvio = enviaParaRD($args);
   // envia email
   $to = 'no-reply@dnadevendas.com.br';
   $subject = 'DNA de Vendas - Contato';
-  $message = "Nome: ".$nome
-      ."<br>Email: ".$email
-      ."<br>urlOrigem: ".$urlOrigem;
+  $message = criaMensagemPaginaDeContato($reqData);
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $wpmail = wp_mail( $to, $subject, $message, $headers );
   $url = $_SERVER['HTTP_ORIGIN'] . '/contato/agradecimento/';
@@ -31,27 +31,24 @@ function dnaapi_contatoFooter($req){
  */
 function dnaapi_solicitarContato($req){
   // pega os parametros
-  $nome = $req->get_param('nome');
-  $email = $req->get_param('email');
-  $mobile_phone = $req->get_param('mobile_phone');
-  $company_name = $req->get_param('company_name');
-  $qtdfunc = $req->get_param('qtdfunc');
-  $urlOrigem = $req->get_param('urlOrigem');
-  /** Como agora quem envia para o RD é o servidor e esse form tem "name" diferentes para
-   * páginas diferentes, preciso receber o name para enviar corretamente para o rd */
-  $nameOfForm = $req->get_param('nameOfForm');
+  $reqData = array(
+    'urlOrigem' => $req->get_param('urlOrigem'),
+    'nome' => $req->get_param('nome'),
+    'email' => $req->get_param('email'),
+    'telefone' => $req->get_param('mobile_phone'),
+    'company_name' => $req->get_param('company_name'),
+    'nVendedores' => $req->get_param('qtdfunc'),
+    /** Como agora quem envia para o RD é o servidor e esse form tem "name" diferentes para
+     * páginas diferentes, preciso receber o name para enviar corretamente para o rd */
+    'nameOfForm' => $req->get_param('nameOfForm')
+  );
   //configura e envia para o rd
-  $args = configuraEnviarParaRDSolicitarContato($nameOfForm, $nome, $email, $mobile_phone, $company_name, $qtdfunc, $urlOrigem);
+  $args = configuraEnviarParaRDSolicitarContato($reqData);
   $statusEnvio = enviaParaRD($args);
   // envia email
   $to = 'forsmall-leads@dnadevendas.com.br';
   $subject = 'DNA de Vendas - Contato';
-  $message = "Nome: ".$nome
-      ."<br>Email: ".$email
-      ."<br>mobile_phone: ".$mobile_phone
-      ."<br>company_name: ".$company_name
-      ."<br>qtdfunc: ".$qtdfunc
-      ."<br>urlOrigem: ".$urlOrigem;
+  $message = criaMensagemPaginaDeContato($reqData);
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $wpmail = wp_mail( $to, $subject, $message, $headers );
   $url = $_SERVER['HTTP_ORIGIN'] . '/contato/agradecimento/';
@@ -65,16 +62,18 @@ function dnaapi_solicitarContato($req){
  */
 function dnaapi_paginaContato($req){
   // pega os parametros
-  $nome = $req->get_param('nome');
-  $email = $req->get_param('email');
-  $setor = $req->get_param('setor');
-  $nVendedores = $req->get_param('nVendedores');
-  $telefone = $req->get_param('telefone');
-  $assunto = $req->get_param('assunto');
-  $mensagem = $req->get_param('mensagem');
-  $urlOrigem = $req->get_param('urlOrigem');
+  $reqData = array(
+    'urlOrigem' => $req->get_param('urlOrigem'),
+    'nome' => $req->get_param('nome'),
+    'email' => $req->get_param('email'),
+    'setor' => $req->get_param('setor'),
+    'nVendedores' => $req->get_param('nVendedores'),
+    'telefone' => $req->get_param('telefone'),
+    'assunto' => $req->get_param('assunto'),
+    'mensagem' => $req->get_param('mensagem')
+  );
   //configura e envia para o rd
-  $args = configuraEnviarParaRDPaginaContato($nome, $email, $setor, $nVendedores, $telefone, $assunto, $mensagem, $urlOrigem);
+  $args = configuraEnviarParaRDPaginaContato($reqData);
   $statusEnvio = enviaParaRD($args);
   // envia email
   $to = array('contato@dnadevendas.com.br>',
@@ -82,7 +81,7 @@ function dnaapi_paginaContato($req){
       'lucia@dnadevendas.com.br>'
     );
   $subject = 'DNA de Vendas - Contato';
-  $message = criaMensagemPáginaDeContato($nome, $email, $setor, $nVendedores, $telefone, $assunto, $mensagem, $urlOrigem);
+  $message = criaMensagemPaginaDeContato($reqData);
   $headers = array('Content-Type: text/html; charset=UTF-8');
   $wpmail = wp_mail( $to, $subject, $message, $headers );
   // redireciona a página
@@ -175,56 +174,45 @@ add_action('rest_api_init', 'dnaapi_register_ccp');
 /**
  * Configura os campos para ir para o rd
  * 
- * @param string $nome - campo nome do formulário
- * @param string $email - campo email do formulário
- * @param string $mobile_phone - campo mobile_phone do formulário
- * @param string $company_name - campo company_name do formulário
- * @param string $qtdfunc - campo qtdfunc do formulário
- * @param string $urlOrigem - campo urlOrigem do formulário
+ * @param array $data - informações do formulário
  * @return object - objeto preparado para envio
  * @author Vinicius de Santana
 */
-function configuraEnviarParaRDNewsletterFooter($nome, $email, $urlOrigem){
+function configuraEnviarParaRDNewsletterFooter($data){
   $args = new stdClass();
   $args->event_type = "CONVERSION";
   $args->event_family = "CDP";
   $args->payload = new stdClass();
   $args->payload->conversion_identifier = "dna_newsletter";
-  //a partir daqui é o conteúdo do form
-  $args->payload->name = $nome;
-  $args->payload->email = $email;
-  $args->payload->cf_urlorigem = $urlOrigem;
+  //conteúdo do form
+  $args->payload->name = $data['nome'];
+  $args->payload->email = $data['email'];
+  $args->payload->cf_urlorigem = $data['urlOrigem'];
   return $args;
 }
 
 /**
  * Configura os campos para ir para o rd
  * 
- * @param string $nameOfForm - Esse form existe em várias páginas,  preciso dessa informação para indentificar a conversão
- * @param string $nome - campo nome do formulário
- * @param string $email - campo email do formulário
- * @param string $mobile_phone - campo mobile_phone do formulário
- * @param string $company_name - campo company_name do formulário
- * @param string $qtdfunc - campo qtdfunc do formulário
- * @param string $urlOrigem - campo urlOrigem do formulário
+ * @param array $data - informações do formulário
  * @return object - objeto preparado para envio
  * @author Vinicius de Santana
 */
-function configuraEnviarParaRDSolicitarContato($nameOfForm, $nome, $email, $mobile_phone, $company_name, $qtdfunc, $urlOrigem){
+function configuraEnviarParaRDSolicitarContato($data){
   $args = new stdClass();
   $args->event_type = "CONVERSION";
   $args->event_family = "CDP";
   $args->payload = new stdClass();
   /** Como agora quem envia para o RD é o servidor e esse form tem "name" diferentes para
    * páginas diferentes, preciso receber o name para enviar corretamente para o rd */
-  $args->payload->conversion_identifier = $nameOfForm;
+  $args->payload->conversion_identifier = $data['nameOfForm'];
   //a partir daqui é o conteúdo do form
-  $args->payload->name = $nome;
-  $args->payload->email = $email;
-  $args->payload->mobile_phone = $mobile_phone;
-  $args->payload->company_name = $company_name;
-  $args->payload->cf_vendedores_na_equipe = $qtdfunc;
-  $args->payload->cf_urlorigem = $urlOrigem;
+  $args->payload->name = $data['nome'];
+  $args->payload->email = $data['email'];
+  $args->payload->mobile_phone = $data['telefone'];
+  $args->payload->company_name = $data['company_name'];
+  $args->payload->cf_vendedores_na_equipe = $data['nVendedores'];
+  $args->payload->cf_urlorigem = $data['urlOrigem'];
   return $args;
 }
 
@@ -233,18 +221,11 @@ function configuraEnviarParaRDSolicitarContato($nameOfForm, $nome, $email, $mobi
 /**
  * Cria a mensagem dá página contato com os parametros passados
  * 
- * @param string $nome - campo nome do formulário
- * @param string $email - campo email do formulário
- * @param string $setor - campo setor do formulário
- * @param string $nVendedores - campo nVendedores do formulário
- * @param string $telefone - campo telefone do formulário
- * @param string $assunto - campo assunto do formulário
- * @param string $mensagem - campo mensagem do formulário
- * @param string $urlOrigem - campo urlOrigem do formulário
+ * @param array $data - informações do formulário
  * @return string  - o html para enviar o email
  * @author Vinicius de Santana
 */
-function criaMensagemPáginaDeContato($nome, $email, $setor, $nVendedores, $telefone, $assunto, $mensagem, $urlOrigem){
+function criaMensagemPaginaDeContato($data){
   // inicio mensagem
   $message = '<div style="font-family: Arial, sans-serif;margin: 40px auto;width: 550px;">';
   $message .=   '<table>';
@@ -256,56 +237,80 @@ function criaMensagemPáginaDeContato($nome, $email, $setor, $nVendedores, $tele
   $message .=   '</table>';
   $message .=   '<table style="font-family: Arial, sans-serif;background-color: #eee;margin: 20px 40px 50px 40px;border-radius: 6px;min-width: 450px;">';
   $message .=     '<tbody>';
-  $message .=     '<tr>';
-  $message .=       '<td style="padding: 10px 20px;width: 60px">Nome:</td>';
-  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $nome;
-  $message .=       '</td>';
-  $message .=     '</tr>';
-  $message .=     '<tr>';
-  $message .=       '<td style="padding: 10px 20px;">E-mail:</td>';
-  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $email;
-  $message .=       '</td>';
-  $message .=     '</tr>';
-  $message .=     '<tr>';
-  $message .=       '<td style="padding: 10px 20px;">Falar com setor:</td>';
-  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $setor;
-  $message .=       '</td>';
-  $message .=     '</tr>';
-  $message .=     '<tr>';
-  $message .=       '<td style="padding: 10px 20px;">Vendedores:</td>';
-  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $nVendedores;
-  $message .=       '</td>';
-  $message .=     '</tr>';
+  if (isset($data['nome'])){
+    $message .=     '<tr>';
+    $message .=       '<td style="padding: 10px 20px;width: 60px">Nome:</td>';
+    $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+    $message .=         $data['nome'];
+    $message .=       '</td>';
+    $message .=     '</tr>';
+  }
+  if (isset($data['email'])){
+    $message .=     '<tr>';
+    $message .=       '<td style="padding: 10px 20px;">E-mail:</td>';
+    $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+    $message .=         $data['email']; // obrigatório
+    $message .=       '</td>';
+    $message .=     '</tr>';
+  }
+  if (isset($data['setor'])){
+    $message .=     '<tr>';
+    $message .=       '<td style="padding: 10px 20px;">Falar com setor:</td>';
+    $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+    $message .=         $data['setor'];
+    $message .=       '</td>';
+    $message .=     '</tr>';
+  }
+  if (isset($data['nVendedores'])){
+    $message .=     '<tr>';
+    $message .=       '<td style="padding: 10px 20px;">Vendedores:</td>';
+    $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+    $message .=         $data['nVendedores'];
+    $message .=       '</td>';
+    $message .=     '</tr>';
+  }
+  if (isset($data['telefone'])){
   $message .=     '<tr>';
   $message .=       '<td style="padding: 10px 20px;">Telefone:</td>';
   $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $telefone;
+  $message .=         $data['telefone'];
   $message .=       '</td>';
   $message .=     '</tr>';
+  }
+  if (isset($data['assunto'])){
   $message .=     '<tr>';
   $message .=       '<td style="padding: 10px 20px;">Assunto:</td>';
   $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $assunto;
+  $message .=         $data['assunto'];
   $message .=       '</td>';
   $message .=     '</tr>';
+  }
+  if (isset($data['mensagem'])){
   $message .=     '<tr>';
   $message .=       '<td style="padding: 10px 20px;" colspan="2">';
 	$message .=         '<p style="padding-bottom: 20px;">Mensagem:</p>';
   $message .=         '<div style="font-size: 18px;">';
-  $message .=            $mensagem;
+  $message .=            $data['mensagem'];
   $message .=          '</div>';
   $message .=       '</td>';
   $message .=     '</tr>';
-  $message .=     '<tr>';
-  $message .=       '<td style="padding: 10px 20px;">UrlOrigem:</td>';
-  $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
-  $message .=         $urlOrigem;
-  $message .=       '</td>';
-  $message .=     '</tr>';
+  }
+  if (isset($data['company_name'])){
+    $message .=     '<tr>';
+    $message .=       '<td style="padding: 10px 20px;">Nome da Empresa:</td>';
+    $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+    $message .=         $data['company_name'];
+    $message .=       '</td>';
+    $message .=     '</tr>';
+  }
+  if (isset($data['urlOrigem'])){
+    $message .=     '<tr>';
+    $message .=       '<td style="padding: 10px 20px;">UrlOrigem:</td>';
+    $message .=       '<td style="padding: 10px 20px; font-size: 18px;">';
+    $message .=         $data['urlOrigem']; // obrigatório
+    $message .=       '</td>';
+    $message .=     '</tr>';
+  }
   $message .=     '</tbody>';
   $message .=   '</table>';
   $message .=   '<p style="font-family: Arial, sans-serif;font-weight: 100;font-style: italic;">Enviado pelo site Consultoria DNA de Vendas.</p>';
@@ -317,32 +322,25 @@ function criaMensagemPáginaDeContato($nome, $email, $setor, $nVendedores, $tele
 /**
  * Configura os campos para ir para o rd
  * 
- * @param string $nome - campo nome do formulário
- * @param string $email - campo email do formulário
- * @param string $setor - campo setor do formulário
- * @param string $nVendedores - campo nVendedores do formulário
- * @param string $telefone - campo telefone do formulário
- * @param string $assunto - campo assunto do formulário
- * @param string $mensagem - campo mensagem do formulário
- * @param string $urlOrigem - campo urlOrigem do formulário
+ * @param array $data - informações do formulário
  * @return object - objeto preparado para envio
  * @author Vinicius de Santana
 */
-function configuraEnviarParaRDPaginaContato($nome, $email, $setor, $nVendedores, $telefone, $assunto, $mensagem, $urlOrigem){
+function configuraEnviarParaRDPaginaContato($data){
   $args = new stdClass();
   $args->event_type = "CONVERSION";
   $args->event_family = "CDP";
   $args->payload = new stdClass();
   $args->payload->conversion_identifier = "/contato"."/";
   //a partir daqui é o conteúdo do form
-  $args->payload->name = $nome;
-  $args->payload->email = $email;
-  $args->payload->cf_setor = $setor;
-  $args->payload->cf_vendedores_na_equipe = $nVendedores;
-  $args->payload->mobile_phone = $telefone;
-  $args->payload->cf_assunto = $assunto;
-  $args->payload->cf_urlorigem = $urlOrigem;
-  $args->payload->cf_mensagem = $mensagem;
+  $args->payload->name = $data['nome'];
+  $args->payload->email = $data['email'];
+  $args->payload->cf_setor = $data['setor'];
+  $args->payload->cf_vendedores_na_equipe = $data['nVendedores'];
+  $args->payload->mobile_phone = $data['telefone'];
+  $args->payload->cf_assunto = $data['assunto'];
+  $args->payload->cf_urlorigem = $data['urlOrigem'];
+  $args->payload->cf_mensagem = $data['mensagem'];
   return $args;
 }
 
