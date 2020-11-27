c7f77cd283d32cacfa6c763d49d7eec4f69f6a1d
Vinicius de Santana <vinicius.vts@gmail.com>
2020-11-27 16:40:35 -0300

    feat: custom post banner home

---
+++ wp-content/themes/dna/custom-posts/index.php
@@ -0,0 +1,2 @@
+<?php
+  include_once 'banner-home.php';
--- wp-content/themes/dna/functions.php
+++ wp-content/themes/dna/functions.php
@@ -3,6 +3,7 @@
   define('POST_META_COUNT', 'dna_views');
   // imports
   include_once 'api/index.php';
+  include_once 'custom-posts/index.php';
   include_once 'inc/index.php';
   // contact form estava marcando como spam qualquer requisição ao endpoind rest api
   add_filter('wpcf7_spam', '__return_false');
